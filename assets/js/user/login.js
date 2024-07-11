9d8dee9350ee15d88de1af69eef8482cce95237d
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-11 19:54:10 +0700

    perbagus login dengan sweetaler

--- assets/js/user/login.js
+++ assets/js/user/login.js
@@ -1,39 +1,53 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'https://asia-southeast2-proven-wavelet-401905.cloudfunctions.net/loginai';
-  
-    document.getElementById('loginBtn').addEventListener('click', function() {
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
-  
-      const loginData = {
-        username: username,
-        password: password
-      };
-  
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(loginData)
-      };
-  
-      fetch(apiUrl, requestOptions)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => {
-          console.log('Login successful:', data);
-          alert('Login successful!');
-           window.location.href = '../fitur/chat.html';
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          document.getElementById('error-message').textContent = 'Login failed. Please check your username and password.';
-        });
+import { getValue } from "https://jscroot.github.io/element/croot.js";
+
+function postLogin(target_url, data, responseFunction) {
+  const requestOptions = {
+    method: "POST",
+    body: JSON.stringify(data),
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => responseFunction(JSON.parse(result)))
+    .catch((error) => console.log("error", error));
+}
+
+const Login = () => {
+  const target_url =
+    "https://asia-southeast2-proven-wavelet-401905.cloudfunctions.net/loginai";
+
+  const data = {
+    email: getValue("username"),
+    password: getValue("password"),
+  };
+
+  postLogin(target_url, data, responseData);
+};
+
+function responseData(result) {
+  if (result.error === undefined || !result.error) {
+    document.cookie = `Authorization=${encodeURIComponent(
+      result.token
+    )}; path=/;`;
+
+    Swal.fire({
+      icon: "success",
+      title: "Login Successful",
+      text: result.message,
+    }).then(() => {
+      window.location.href = "./chat.html";
     });
-  });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Login Failed",
+      text: result.message,
+    });
+  }
+}
+
+document.getElementById("button").addEventListener("click", Login);
+  
+
   
