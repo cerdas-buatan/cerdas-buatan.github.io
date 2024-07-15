58becf2b62739842bd4741633099924e1fe0a349
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-15 20:50:37 +0700

    update chat js

--- assets/js/cht.js
+++ assets/js/cht.js
@@ -90,8 +90,22 @@ function closeNav() {
 }
 
 function logout() {
-    // Implement logout functionality here
-    console.log("Logged out");
+    Swal.fire({
+        title: 'Yakin Mau Log Out?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Iya'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = '../index.html'; // Mengarahkan pengguna ke halaman index.html 
+        }
+    });
+}
+
+function login() {
+    window.location.href = './login.html'; // Mengarahkan pengguna ke halaman login.html 
 }
 
 function scrollToChat() {
