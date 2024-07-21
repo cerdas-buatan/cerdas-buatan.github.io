87865d7cd2173f1883d7f9716cc2c3b13d2be426
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-21 22:34:50 +0700

    updated room chat

--- assets/js/cht.js
+++ assets/js/cht.js
@@ -1,3 +1,15 @@
+// function getcookie(name){
+//     let cookieArr = document.cookie.split(";");
+//     for(let i=0; i< cookieArr.length; i++){
+//         let cookiePair = cookieArr[i].split("=");
+//         if(name == cookiePair[0].trim()){
+//             return decodeURIComponent(cookiePair[1]);
+//         }
+//     }
+//     return null;
+// }
+
+
 document.getElementById('user-input').addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
         sendMessage();
