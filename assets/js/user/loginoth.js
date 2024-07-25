5aaf4a6f415c6cfca71ada9208be56c074be1d45
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-25 17:37:07 +0700

    add func post

--- assets/js/user/loginoth.js
+++ assets/js/user/loginoth.js
@@ -1,2 +1,15 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
+
+function postWithToken(target_url, datajson, responseFunction) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+  
+    var raw = JSON.stringify(datajson);
+  
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
