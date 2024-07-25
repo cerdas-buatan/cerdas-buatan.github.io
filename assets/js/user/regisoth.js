11551549625274f676afb4e2adcd8c419b0b42c7
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-25 17:42:03 +0700

    add async function postRegister

--- assets/js/user/regisoth.js
+++ assets/js/user/regisoth.js
@@ -1,2 +1,12 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 
+async function postRegister(target_url, data) {
+    try {
+      const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow'
+      };
