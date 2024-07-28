f928ad43d2590e35bd4748568b045b3a1b34a8e1
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-28 09:33:06 +0700

    modify

--- assets/js/cht.js
+++ assets/js/cht.js
@@ -130,10 +130,5 @@ document.addEventListener('DOMContentLoaded', () => {
     checkLoginStatus();
 });
 
-// Utility function to get cookie value by name
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
+
 
