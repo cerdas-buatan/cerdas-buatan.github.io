f8719191d1e3a79f4f89a438da872a46b59bb45c
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-07-28 09:33:41 +0700

    modify1

--- assets/js/cht.js
+++ assets/js/cht.js
@@ -9,6 +9,36 @@
 //     return null;
 // }
 
+// Function to toggle button visibility based on login status
+function checkLoginStatus() {
+    const userLogin = getCookie("user_login");
+    const loginButton = document.querySelector(".sidebar-buttons .sidebar-btn");
+    const logoutButton = document.querySelector("#logoutButton");
+    const profilePicture = document.querySelector("#profile-picture");
+
+    if (userLogin) {
+        loginButton.style.display = "none";
+        logoutButton.style.display = "block";
+        profilePicture.style.display = "block";
+        profilePicture.src = localStorage.getItem('profilePicture'); // Set profile picture from localStorage
+    } else {
+        loginButton.style.display = "block";
+        logoutButton.style.display = "none";
+        profilePicture.style.display = "none";
+        profilePicture.src = ""; // Clear profile picture
+    }
+}
+
+// DOMContentLoaded event to set welcome message and check login status
+document.addEventListener('DOMContentLoaded', () => {
+    const username = localStorage.getItem('username');
+    if (username) {
+        const defaultTextContainer = document.querySelector('.default-text');
+        defaultTextContainer.innerHTML = `<h1>Selamat Datang, ${username}</h1>`;
+    }
+    checkLoginStatus();
+});
+
 
 document.getElementById('user-input').addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
@@ -100,35 +130,6 @@ function logout() {
     });
 }
 
-// Function to toggle button visibility based on login status
-function checkLoginStatus() {
-    const userLogin = getCookie("user_login");
-    const loginButton = document.querySelector(".sidebar-buttons .sidebar-btn");
-    const logoutButton = document.querySelector("#logoutButton");
-    const profilePicture = document.querySelector("#profile-picture");
-
-    if (userLogin) {
-        loginButton.style.display = "none";
-        logoutButton.style.display = "block";
-        profilePicture.style.display = "block";
-        profilePicture.src = localStorage.getItem('profilePicture'); // Set profile picture from localStorage
-    } else {
-        loginButton.style.display = "block";
-        logoutButton.style.display = "none";
-        profilePicture.style.display = "none";
-        profilePicture.src = ""; // Clear profile picture
-    }
-}
-
-// DOMContentLoaded event to set welcome message and check login status
-document.addEventListener('DOMContentLoaded', () => {
-    const username = localStorage.getItem('username');
-    if (username) {
-        const defaultTextContainer = document.querySelector('.default-text');
-        defaultTextContainer.innerHTML = `<h1>Selamat Datang, ${username}</h1>`;
-    }
-    checkLoginStatus();
-});
 
 
 
